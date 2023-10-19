4621057d92aa7f7a4d39f2ec12aaed289d0e5b1c
Gabriel Lopes <gabriellopes9135@gmail.com>
2023-10-18 22:05:10 -0300

    18/10/2023

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => ['nome'=>'Nescal', 'status'=>'1'],
+            1 => ['nome'=>'Toddy', 'status'=>'2'],
+        ];
+        return view('app.fornecedor.index', [
+            'fornecedores'=>$fornecedores        
+        ]);
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,21 @@
 <h1>Fornecedores</h1>
 
-{{--Fica o Comentario que nao será interpretado --}}
-
-
 @php
-    
+    $status = ['','Ativo', 'Inativo']
 @endphp
+
+@foreach ($fornecedores as $fornecedor)
+    Fornecedor: {{$fornecedor['nome']}} <br>
+    Status: {{$status[$fornecedor['status']]}} <br>
+@endforeach
+
+
+{{-- @if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3> Existem alguns fornecedores </h3>
+@elseif(count($fornecedores) > 10)
+    <h3> Existem Muitos fornecedores </h3>
+@else
+    <h3> Nenhum Fornecedor Cadastrado </h3> 
+@endif --}}
 
 
