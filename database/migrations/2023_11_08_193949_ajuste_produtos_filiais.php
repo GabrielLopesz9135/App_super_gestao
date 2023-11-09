ba13521e75a18401e9faf5a3ab908630fc453dc9
Gabriel Lopes <gabriellopes9135@gmail.com>
2023-11-09 08:21:43 -0300

    Migration Produtos

---
+++ database/migrations/2023_11_08_193949_ajuste_produtos_filiais.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('filiais', function (Blueprint $table){
+            $table->id();
+            $table->string('filial', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('produto_filiais', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('filial_id');
+            $table->unsignedBigInteger('produto_id ');
+            $table->decimal('preco_venda', 8, 2);
+            $table->integer('estoque_minimo');
+            $table->integer('estoque_maximo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
